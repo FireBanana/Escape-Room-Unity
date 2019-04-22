52cd572eb55220d9bb605e032b45c0391414d9cd
Fire Banana <free0run@gmail.com>
2019-04-22 22:37:16 +0500

    Updates

---
+++ Assets/Scripts/AnswerManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerManager : MonoBehaviour
+{
+    public static AnswerManager Instance;
+    
+    [HideInInspector] public int Score;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void AnswerQuestion(bool isCorrect)
+    {
+        Score = isCorrect ? Score + 100 : Score - 200;
+        //Send Packet
+    }
+    
+    
+}
---
+++ Assets/Scripts/QuestionHolder.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class QuestionHolder : MonoBehaviour
+{
+    [Header("Question Bodies")] public List<GameObject> QuestionBodies;
+    private int currentActiveBodyId = -1;
+
+    public void SelectQuestion(int id)
+    {
+        if (currentActiveBodyId == id)
+        {
+            MinimizeBody(id);
+            currentActiveBodyId = -1;
+        }
+        else
+        {
+            if (currentActiveBodyId != -1)
+            {
+                MinimizeBody(currentActiveBodyId);
+            }
+            
+            MaximizeBody(id);
+            currentActiveBodyId = id;
+        }
+    }
+
+    void MinimizeBody(int id)
+    {
+        var element = QuestionBodies[id].GetComponent<RectTransform>();
+        element.DOSizeDelta(new Vector2(element.sizeDelta.x, 0), 0.5f);
+    }
+
+    void MaximizeBody(int id)
+    {
+        var element = QuestionBodies[id].GetComponent<RectTransform>();
+        element.DOSizeDelta(new Vector2(element.sizeDelta.x, 254), 0.5f);
+    }
+}
